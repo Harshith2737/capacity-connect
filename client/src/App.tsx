6f4ebf8550bf3800869d4c6acc9032128790af6b
Harshith2737 <harshithgovada34@gmail.com>
2026-09-11 23:25:39 +0530

    fix: add real role-based portal routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,26 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
+import Portal, { PortalRole } from "./pages/Portal";
+
+function PortalRoute({ role }: { role: PortalRole }) {
+  return <Portal role={role} />;
+}
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+
+      <Route path="/trainee" component={() => <PortalRoute role="trainee" />} />
+      <Route path="/trainee/:rest*" component={() => <PortalRoute role="trainee" />} />
+
+      <Route path="/trainer" component={() => <PortalRoute role="trainer" />} />
+      <Route path="/trainer/:rest*" component={() => <PortalRoute role="trainer" />} />
+
+      <Route path="/admin" component={() => <PortalRoute role="admin" />} />
+      <Route path="/admin/:rest*" component={() => <PortalRoute role="admin" />} />
+
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
